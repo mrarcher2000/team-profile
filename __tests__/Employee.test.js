739bc4b622833de57d29ed4048da93e76be74eaa
unknown <archernich@gmail.com>
2021-04-10 00:53:31 -0700

    first test layout and Employee obj start

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,12 @@
+const { expect, test } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
+
+test ('create an Employee obj', () => {
+    const employee = new Employee('Bob', 2, 'bob@bobmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
 
